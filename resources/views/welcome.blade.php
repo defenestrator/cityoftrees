1c9d1e08e8872370c0fd203ec203fd3d5ad6b941
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2019-07-08 14:49:15 -0600

    better design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,132 +1,139 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,400|Playfair+Display:400,900&display=swap&subset=latin-ext" rel="stylesheet">
-        <!-- Styles -->
-        <style>
-            html, body {
-                background: white;
-                color: white;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                margin: 0;
-            }
+    <title>{{ config('app.name') }}</title>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,400|Playfair+Display:400,900&display=swap&subset=latin-ext"
+        rel="stylesheet">
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background: white;
+            color: #353939;
+            font-family: 'Josefin Sans', sans-serif;
+            font-weight: 100;
+            margin: 0;
+        }
 
-            .twenty-vh {
-                height:20vh;
-            }
+        .full-height {
+            height: 80vh;
+        }
 
-            .sunset {
-                background: linear-gradient(rgba(55,141,202,1) 0%, rgba(126,192,238,1) 25%, rgba(252,176,69,1) 50% , #ff8739 75%);
-            }
-            .mask {
-                background:transparent linear-gradient(90deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.0) 50%, rgba(0, 0, 0, 0.5) 100%);
-            }
-            .forest{
-                background:transparent url('/img/forest.png') no-repeat bottom right;
-                background-size:cover;
-                background-position-x:bottom;
-            }
+        .twenty-vh {
+            height: 20vh;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .sunset {
+            background: #ffffff;
+        }
 
-            .flex-top {
-                align-items: top;
-                display: flex;
-                justify-content: center;
-            }
-            .position-ref {
-                position: relative;
-            }
+        .mask {
+            background: transparent linear-gradient(90deg, rgba(255, 255, 255, 0.9) 0%, rgba(255, 255, 255, 0.0) 50%, rgba(255, 255, 255, 0.9) 100%);
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .forest {
+            background: transparent url('/img/forest-honeydew.png') no-repeat bottom right;
+            background-size: cover;
+            background-position-x: bottom;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .title {
-                font-size: 64px;
-                font-family:'Playfair Display', serif;
-                color:white;
-                font-weight:900;
-            }
+        .flex-top {
+            align-items: top;
+            display: flex;
+            justify-content: center;
+        }
 
-            .links > a {
-                color: white;
-                padding: 12.5px;
-                font-size: 13px;
-                font-weight: 600;
-                line-height:2em;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-                font-weight:900;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .m-b-md {
-                margin: 30px 0;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .bottom-center {
-                position:absolute;
-                bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="position-ref full-height sunset">
-            <div class="full-height forest">
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 64px;
+            font-family: 'Playfair Display', serif;
+            color: #353839;
+            font-weight: 900;
+        }
+
+        .links>a {
+            color: #353839;
+            padding: 12.5px;
+            font-size: 13px;
+            font-weight: 600;
+            line-height: 2em;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+            font-weight: 900;
+        }
+
+        .m-b-md {
+            margin: 30px 0;
+        }
+
+        .fixed-bottom {
+            position: fixed;
+            bottom: 0px;
+            background: black;
+            color:white;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="position-ref full-height sunset">
+        <div class="full-height forest">
             <div class="flex-center full-height mask">
-            <div class="top-right links">
+                <div class="top-right links">
                     @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                    <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="#">Shop</a>
-                        <a href="{{ route('login') }}">Login</a>
-                        @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                        @endif
+                    <a href="#">Shop</a>
+                    <a href="{{ route('login') }}">Login</a>
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                    @endif
                     @endauth
-                @endif
-
-            </div>
-            <div class="content">
-                <div class="title m-b-md">
-                    {{ config('app.name') }}<br>CBD
+                    @endif
                 </div>
-            </div>
-        </div>
-        </div>
-        {{-- <div class="position-ref flex-top twenty-vh" style="background-color:white; color:#0f0f0f;">
-            <div class="content">
-                <h2 style="">CBD for your health</h2>
-            </div>
-        </div>
-        <div class="position-ref full-height flex-top" style="background-color:#0f0f0f; color:#fff;">
                 <div class="content">
-                    <h2 style="">Dr. Thomas Wiggins M.D.</h2>
+                    <div class="title m-b-md">
+                        {{ config('app.name') }}<br>CBD
+                    </div>
                 </div>
-            </div> --}}
+            </div>
+            <div class="position-ref twenty-vh flex-top" style="padding:48px 0; background-color:#353839; color:#fff;">
+                <div class="content">
+                    <h2 style="">Designed in cooperation with Dr. Thomas Wiggins M.D.</h2>
+                    <h3>Grand Opening &mdash; Summer 2019</h3>
+                </div>
+            </div>
+        </div>
     </div>
-    </body>
+</body>
+
 </html>
