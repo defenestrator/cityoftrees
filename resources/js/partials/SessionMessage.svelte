0e8ac1b6c7b57b8a0f9bc481e15e6fb919a76acd
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-08-01 14:09:26 -0600

    product detail page, flash messaging stubs, shit like that.

---
+++ resources/js/partials/SessionMessage.svelte
@@ -0,0 +1,60 @@
+<div>
+{#each messages as message}
+    {#if message.type === 'error'}
+    <div class="bg-red-200 text-red-900 rounded-lg shadow-md p-6 pr-10">
+        <button
+            class="opacity-75 cursor-pointer absolute top-0 right-0 py-2 px-3 hover:opacity-100"
+            style="min-width: 240px"
+        >
+            ×
+        </button>
+        <div class="flex items-center">
+            { message.text }
+        </div>
+    </div>
+    {/if}
+    {#if message.type === 'success'}
+    <div class="bg-green-200 text-green-900 rounded-lg shadow-md p-6 pr-10">
+        <button
+            class="opacity-75 cursor-pointer absolute top-0 right-0 py-2 px-3 hover:opacity-100"
+            style="min-width: 240px"
+        >
+            ×
+        </button>
+        <div class="flex items-center">
+            { message.text }
+        </div>
+    </div>
+    {/if}
+    {#if message.type === 'warning'}
+    <div class="bg-yellow-200 text-yellow-900 rounded-lg shadow-md p-6 pr-10">
+        <button
+            class="opacity-75 cursor-pointer absolute top-0 right-0 py-2 px-3 hover:opacity-100"
+            style="min-width: 240px"
+        >
+            ×
+        </button>
+        <div class="flex items-center">
+            { message.text }
+        </div>
+    </div>
+    {/if}
+    {#if message.type != 'success' && message.type != 'warning' && message.type != 'error'}
+    <div class="bg-gray-200 text-gray-900 rounded-lg shadow-md p-6 pr-10">
+        <button
+            class="opacity-75 cursor-pointer absolute top-0 right-0 py-2 px-3 hover:opacity-100"
+            style="min-width: 240px"
+        >
+            ×
+        </button>
+        <div class="flex items-center">
+            { message.text }
+        </div>
+    </div>
+    {/if}
+{/each}
+</div>
+<script>
+export let messages = null;
+
+</script>
