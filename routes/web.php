5e5f5c7d621804b05ce020d73153b7f719000b2d
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-08-02 09:45:04 -0600

    jesus fucking tits laravel

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
 
 Auth::routes(['verify' => true]);
 
