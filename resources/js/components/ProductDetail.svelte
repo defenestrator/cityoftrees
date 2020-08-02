102905c9eb6f75daaacaf82ff291f394ca0b59a6
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-08-01 19:21:43 -0600

    lighter border

--- resources/js/components/ProductDetail.svelte
+++ resources/js/components/ProductDetail.svelte
@@ -7,17 +7,17 @@ export let product = productData
     <div class="flex-1 px-6">
         {#if product.images.length >= 1}
         {#each product.images as image, index}
-            <img src="{image.medium}" alt="{product.name}" style="width:100%; min-width:240px;" class="pb-4 rounded">
+            <img src="{image.medium}" alt="{product.name}" style="width:100%; min-width:240px; max-width:640px;" class="pb-4 rounded">
         {/each}
         {/if}
     </div>
-    <div class="flex-1 p-6 bg-white border-2 border-gray-900 rounded-lg shadow" style="min-width:240px;">
+    <div class="flex-1 p-6 bg-white border-2 rounded-lg shadow" style="min-width:240px; max-width:640px;">
         <h1 class="text-2xl">{product.name}</h1>
         <p class="py-4">{product.description}</p>
         <p class="text-2xl">${product.price}</p>
         <div class="py-4 text-2xl">
             <button class="btn btn-green" style="background-color:#6ac318;">BUY NOW</button>
-        </div>
+        </div>Í
         <p class="py-2">In Stock: {product.stock}</p>
         <p class="py-2">Contents: {product.contents}</p>
         <p class="py-2">Weight: {product.weight} grams</p>
