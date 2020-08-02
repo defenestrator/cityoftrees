960b39320d1e5d6e4388238aff8822f566cec4a5
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-08-02 08:52:21 -0600

    styleee

--- resources/js/components/Shop.svelte
+++ resources/js/components/Shop.svelte
@@ -30,29 +30,35 @@ function buyNow(uuid) {
         <div class="flex flex-wrap flex-row-reverse content-start w-100">
         {#each products as product}
             <div class="flex-1 rounded border shadow content-center bg-white m-3 py-3 px-2" style="min-width:240px;">
-                <div class="py-2 font-serif font-bold text-xl" style="letter-spacing:0.11rem; font-size:1.375rem;">
+                <div class="py-2 font-serif font-bold text-xl" style="letter-spacing:0.11rem; font-size:1.375rem; border-bottom:2px solid rgba(188, 188, 188, 0.5);">
                     <a href="/products/{product.uuid}">
                         {product.name}
                     </a>
                 </div>
-                <hr>
                 {#if product.images.length >= 1}
                 <div class="m-0 p-0 w-auto pb-2">
                      <a href="/products/{product.uuid}">
-                        <img class="rounded w-auto" style="margin: 0 auto; max-width:300px; width:100%;" src="{product.images[0].square}"alt="Image of {product.name}" title="Image of {product.name}" />
+                        <img
+                            class="rounded w-auto"
+                            style="margin: 0 auto; max-width:300px; width:100%;"
+                            src="{product.images[0].square}"alt="Image of {product.name}"
+                            title="Image of {product.name}"
+                        />
                     </a>
                 </div>
                 {/if}
                 <div class="py-2" style="letter-spacing:0.11rem; font-size:1.375rem;"> ${product.price}</div>
                 <div class="py-2 w-auto">
-                    <button on:click="{buyNow(product.uuid)}" class="btn btn-green" style="background-color:#6ac318;">&nbsp; &nbsp; BUY NOW &nbsp; &nbsp;</button>
+                    <button on:click="{buyNow(product.uuid)}" class="btn btn-green" style="letter-spacing:0.11rem;">
+                        &nbsp; BUY NOW &nbsp;
+                    </button>
                 </div>
-                <div class="py-4 w-auto text-justify" style="height:5rem; overflow:hidden; border-bottom:2px solid rgba(188, 188, 188, 0.6);">
-                    <p class="py-2" style="text-overflow: ellipsis; font-size:1.125rem;"><a href="/products/{product.uuid}">{product.description}</a></p>
+                <div class="py-4 w-auto shop-description">
+                    <a href="/products/{product.uuid}">{product.description}</a>
                 </div>
                 <div class="py-4 w-auto">
                     <a href="/products/{product.uuid}">
-                        <button class="btn btn-green text-white" style="background-color:#f6ad55;">LEARN MORE</button>
+                        <button class="btn btn-orange">LEARN MORE</button>
                     </a>
                 </div>
 
